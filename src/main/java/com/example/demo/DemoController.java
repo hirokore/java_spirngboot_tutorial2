3e55f25c0b3cc0f8c0ef1bfc1029bf84084c2ef7
hiroyuki_yamashita <hirokore3221@gmail.com>
2022-04-20 21:20:10 +0900

    view

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -6,7 +6,17 @@ import org.springframework.stereotype.Controller;
 @Controller //コントローラーとして役割を持たせたクラスファイルという意味を持たせる注釈
 public class DemoController {
 	@RequestMapping("/")//リクエストURLに対して、どのメソッドが処理するかを決める注釈
-	String home() {//homeメソッドをString型で定義して、表示したい文字列をreturnする。
-		return "Hello Spring";
+	public String index() {//homeメソッドをString型で定義して、表示したい文字列をreturnする。
+		return "Index";
 	}
 }
+
+// @Controllerの説明URL いくつか書き方の種類があったのでリクエストの種類が増えたら再読
+// https://pointsandlines.jp/server-side/java/spring-boot-controller-request-mapping
+
+// STS 4.10.0から「Web Developer Tools」が削除されている
+// Eclipse マーケットプレースから、以下をダウンロードすると解決できる
+// Eclipse Enterprise Java and Web Developer Tools
+
+
+//テンプレートエンジンとはデータとテンプレートを合体させ、文字列を作る仕組みのこと
